3b0f93da600ea939ed8faf2d57c6ad80b23ce915
Steve Frost <stevefrost83@gmail.com>
2020-12-19 13:37:29 -0900

    added sudo to lines that required it

--- files/rh-csa-2.sh
+++ files/rh-csa-2.sh
@@ -26,7 +26,7 @@ printf "Checking for user $username....\t\t"; sleep 2; [[ $(sudo cat /etc/passwd
 printf "Checking $username's password....\t\t"; sleep 2; [[ $(sudo cat /etc/shadow | grep $username | awk -F: '{print $2}') == $userhash ]] && $success  || $failure
 printf "Checking $username's ssh keys\t\t"; sleep 2; [[ $(ls /home/$username/.ssh/$keyname 2>/dev/null | wc -l) -eq 2 ]] && $success  || $failure
 printf "Checking $username's sudo rights....\t"; sleep 2; [[ $(sudo id $username | egrep -o "sudo" | wc -w) -eq 1 ]] && $success  || $failure
-printf "Checking sshd configs....\t\t"; sleep 2; [[ $(sudo egrep -i "^PasswordAuthentication no$" /etc/ssh/sshd_config) ]] && [[ $(egrep -i "^PubKeyAuthentication yes$" /etc/ssh/sshd_config) ]] && $success  || $failure
+printf "Checking sshd configs....\t\t"; sleep 2; [[ $(sudo egrep -i "^PasswordAuthentication no$" /etc/ssh/sshd_config) ]] && [[ $(sudo egrep -i "^PubKeyAuthentication yes$" /etc/ssh/sshd_config) ]] && $success  || $failure
 printf "Checking /etc/hosts....\t\t\t"; sleep 2; [[ $(sudo grep -Ei "^10.10.10.[23]{1}(.*)RH-CSA-[12]{1}$" /etc/hosts | wc -l) == "2" ]] && $success  || $failure
 #printf "Checking repository....\t\t\t"; sleep 2; [[ $(egrep -i " && $success  || $failure
 printf "Checking timezone....\t\t\t"; sleep 2; [[ $(timedatectl | egrep -i "^\W+time ?zone" | grep "America/Anchorage" | wc -l) == "1" ]] && $success  || $failure
